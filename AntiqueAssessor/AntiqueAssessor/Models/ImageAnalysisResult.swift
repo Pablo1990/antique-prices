4406f2db9fe57741ec00775bbaf0edc4b2f93d10
Pablo Vicente Munuera <pablovm1990@gmail.com>
2026-03-03 12:53:29 +0100

    Working with prefixes

--- AntiqueAssessor/AntiqueAssessor/Models/ImageAnalysisResult.swift
+++ AntiqueAssessor/AntiqueAssessor/Models/ImageAnalysisResult.swift
@@ -14,27 +14,12 @@ struct ImageAnalysisResult {
         var terms: [String] = []
 
         if Locale.current.languageCode == "es" {
-
-            // Add category (translate to Spanish)
-            let category_es = translateToSpanish(word: suggestedCategory)
-            terms.append(category_es)
-
-            // Add era/style keywords (translate to Spanish)
-            let eraStyleKeywords_es = eraStyleKeywords.map { translateToSpanish(word: $0) }
-            terms.append(contentsOf: eraStyleKeywords_es)
-
             // Add other keywords (limit to top 2, translate to Spanish)
             let suggestedKeywords_es = suggestedKeywords.prefix(2).map { translateToSpanish(word: $0) }
             terms.append(contentsOf: suggestedKeywords_es)
 
             return terms.joined(separator: " ")
         } else {
-            // Add category
-            terms.append(suggestedCategory)
-
-            // Add era/style keywords
-            terms.append(contentsOf: eraStyleKeywords)
-
             // Add other keywords (limit to top 2)
             terms.append(contentsOf: suggestedKeywords.prefix(2))
 
@@ -44,12 +29,10 @@ struct ImageAnalysisResult {
     
     /// Generate todocoleccion.net search URL
     func generateSearchURL() -> URL? {
-        let baseURL = ""
+        var baseURL = "https://en.todocoleccion.net/orientaprecios/-1/g/reciente/0/1?autocompletado="
         /// If language is Spanish, use Spanish base URL, otherwise use English
         if Locale.current.languageCode == "es" {
-            let baseURL = "https://www.todocoleccion.net/orientaprecios/-1/g/reciente/0/1?autocompletado="
-        } else {
-            let baseURL = "https://en.todocoleccion.net/orientaprecios/-1/g/reciente/0/1?autocompletado="
+            baseURL = "https://www.todocoleccion.net/orientaprecios/-1/g/reciente/0/1?autocompletado="
         }
         
         let query = searchQuery.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
